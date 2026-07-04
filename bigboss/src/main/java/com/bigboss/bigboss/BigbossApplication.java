d0337b2ca2afc321b9b43d3f5b5657b2e2cdc773
suresh sunuguri <sunuguri.suresh@gmail.com>
2026-07-04 15:22:57 -0400

    initial checkin

---
+++ bigboss/src/main/java/com/bigboss/bigboss/BigbossApplication.java
@@ -0,0 +1,13 @@
+package com.bigboss.bigboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbossApplication.class, args);
+	}
+
+}
